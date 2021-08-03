a5cff2e22f8f3ea05ac619938f8ed3bb00a09230
aravind <aravindn341@gmail.com>
2021-08-03 11:15:34 +0530

    first commit

--- jobs/models.py
+++ jobs/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Job(models.Model):
     image=models.ImageField(upload_to='images/')
     sumery=models.CharField(max_length=200)
+    url1=models.CharField(max_length=200)
 
 
 
